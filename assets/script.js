0db854d7ae3038bda4c769289b2ea4580057bda2
S-via <168160925+S-via@users.noreply.github.com>
2024-10-24 23:12:37 -0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+    const messageBox = document.getElementById("messageBox");
+    messageBox.style.display = "block"; // Show the message box
+});
