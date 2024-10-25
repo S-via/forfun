e535095801ad461ffb4983ae67881293537b013b
S-via <168160925+S-via@users.noreply.github.com>
2024-10-24 23:12:52 -0700

    Delete forfun directory

--- forfun/assets/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("loveButton").addEventListener("click", function() {
-    const messageBox = document.getElementById("messageBox");
-    messageBox.style.display = "block"; // Show the message box
-});
