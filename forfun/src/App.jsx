2f01d187e7c31ef67ba69e91dcf138f63bec36e4
S-via <168160925+S-via@users.noreply.github.com>
2024-12-03 16:21:59 -0800

    turn app to react app

---
+++ forfun/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // inicial state would be false untill it is used 
+  const [message, setMessage] = useState(false)
+
+  const buttonHandlerClick = () => {
+    // to set the state true after the button is clicked and show message
+    setMessage(true);
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Hey</h2>
+
+        <p>question</p>
+
+        <button id="loveButton" onClick={buttonHandlerClick}>Click</button>
+        {message && (
+          < div id="messageBox">answer from dev</div>
+        )}
+
+      </div >
+    </>
+  )
+}
+
+export default App
