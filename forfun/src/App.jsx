77976d434a4ec77de853825cb76cd2be81d8c71e
S-via <168160925+S-via@users.noreply.github.com>
2024-12-05 00:37:02 -0800

    push before deplou

--- forfun/src/App.jsx
+++ forfun/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import { Button, Box, Text, Heading, Image, Stack, Card, CardBody,Grid,GridItem } from '@chakra-ui/react';
+import img3 from '/IMG_0614.png';
+import img2 from '/IMG_0471.png';
+import img1 from '/IMG_0161.png';
 
 function App() {
   // inicial state would be false untill it is used 
@@ -9,20 +13,44 @@ function App() {
     // to set the state true after the button is clicked and show message
     setMessage(true);
   }
-// test
+  // test
   return (
     <>
-      <div>
-        <h2>Hey</h2>
+      <Box>
+        <Heading>Happy Third Month (:</Heading>
 
-        <p>question</p>
+        <Button
+          bg="#F687B3"
+          size="lg"
+          onClick={buttonHandlerClick}>Click</Button>
 
-        <button id="loveButton" onClick={buttonHandlerClick}>Click</button>
         {message && (
-          < div id="messageBox">answer from dev</div>
+          <Card maxW="md">
+            <CardBody>
+              <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+              <GridItem colSpan={2}>
+                <Image src={img3} alt='Us' borderRadius="lg" />
+              </GridItem>
+              <GridItem>
+                <Image src={img2} alt='Us'borderRadius="lg" />
+                </GridItem>
+                <GridItem>
+                <Image src={img1} alt='Us' borderRadius="lg"/>
+              </GridItem>
+              </Grid>
+              <Stack mt="4" spacing="5">
+                <Text>90 days fulling each other with warmth and memories, Memories so vivid when I close my eyes
+                  I hear each others laugh to our inner jokes. I smile at the thougth of the love language we have created, a language only we understand filled with unspoken word but endless warmth.
+                  Te amo
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+
         )}
 
-      </div >
+      </Box>
     </>
   )
 }
